1f433cbe080746c5d67817c1df0f82328f0087b8
natebecker11 <natebecker11@gmail.com>
2018-09-30 18:57:15 -0400

    thumbnail progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
 // event listener for making thumb paragraphs and links appear on hover
 document.querySelectorAll('.thumb-box').forEach(item => {
   item.addEventListener('mouseenter', () => {
-    item.querySelectorAll('.thumb-card-double-header').forEach(item => {
+    item.querySelectorAll('.thumb-header').forEach(item => {
       item.style.color = '#ef6461';
     })
+    // item.querySelector('.thumb-card-single-header').style.color = '#ef6461';
+    item.querySelector('.thumb-bg').style.opacity = .8;
+    item.querySelector('.url-logo').style.opacity = 1;
+    item.querySelector('.git-logo').style.opacity = 1;
+    item.querySelector('.thumb-text').style.opacity = 1;
   })
   item.addEventListener('mouseleave', () => {
-    item.querySelectorAll('.thumb-card-double-header').forEach(item => {
+    item.querySelectorAll('.thumb-header').forEach(item => {
       item.style.color = '#ffffff';
     })
+    // item.querySelector('.thumb-card-single-header').style.color = '#ffffff';
+    item.querySelector('.thumb-bg').style.opacity = 1;
+    item.querySelector('.url-logo').style.opacity = 0;
+    item.querySelector('.git-logo').style.opacity = 0;
+    item.querySelector('.thumb-text').style.opacity = 0;
   })
 })
