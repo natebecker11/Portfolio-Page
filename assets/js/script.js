302238cae92a06a3d5e6c4efc0649cd1c46f3a29
natebecker11 <natebecker11@gmail.com>
2018-10-01 11:07:42 -0400

    page transition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+
+
+
 // event listener for making thumb paragraphs and links appear on hover
 document.querySelectorAll('.thumb-box').forEach(item => {
   item.addEventListener('mouseenter', () => {
@@ -21,3 +24,44 @@ document.querySelectorAll('.thumb-box').forEach(item => {
     item.querySelector('.thumb-text').style.opacity = 0;
   })
 })
+
+const delay = interval => {
+  return new Promise(resolve => {
+    setTimeout(resolve, interval);
+  });
+}
+
+// const initializeTrans = (a, b, time) => {
+//   return new Promise((resolve, reject) => {
+//     if (a && b) {
+//       a.forEach(item => {
+//         // console.log(item)
+//         item.classList.add('transparent')
+//       })
+//       setTimeout(resolve(a, b), time)
+//     }
+//     else reject(err);
+//   })
+// }
+
+// transition to different page
+const sectTrans = (oldC, newC) => {
+  let oldBoxes = document.querySelectorAll(oldC);
+  let newBoxes = document.querySelectorAll(newC);
+  // initializeTrans(oldBoxes, newBoxes, 1000)
+  oldBoxes.forEach(item => {
+    item.classList.add('transparent')
+  })
+  delay(500)
+    .then(() => {
+      oldBoxes.forEach(item => {
+        item.classList.add('hidden')
+      })
+    })
+    .then(() => {
+      newBoxes.forEach(item => {
+        item.classList.remove('hidden', 'transparent')
+      })
+    })
+}
+
